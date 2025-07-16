5883b3ff879f744c902a87703499dbc5750fdd91
DonovanSalgadoSalazar <donovis22@gmail.com>
2025-07-15 22:46:34 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
 def calculate_area(radius):
     return 3.14 * radius * radius * radius  # ❌ error de lógica: fórmula incorrecta 
-def vulnerable_function():
-    eval("print('inseguro'2)")  # ❌ uso de eval - vulnerabilidad clásica
+

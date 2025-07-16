367de08c5a41ff5a0c4f1cca37239a8c1847d3dd
DonovanSalgadoSalazar <donovis22@gmail.com>
2025-07-15 22:53:13 -0600

    Resolviendo conflictos en app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-def calculate_area(radius):
-    return 3.14 * radius * radius * radius  # ❌ error de lógica: fórmula incorrecta 
-<<<<<<< HEAD
-=======
-
->>>>>>> 5883b3ff879f744c902a87703499dbc5750fdd91
+def calculate_area(radio):
+    if radio < 0:
+        return None
+    return 3.14 * radio * radio
